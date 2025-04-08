9d606d8bf29f11c47c95ab369233e9c0d3a6ca83
VictorDCh <vdubuschanson@outlook.fr>
2025-04-08 15:04:15 +0200

    correct pitch profile

--- utils/convert_dat_to_dopplight.py
+++ utils/convert_dat_to_dopplight.py
@@ -9,5 +9,8 @@ data = np.loadtxt(data_path, skiprows=1)
 # transpose the data
 data = data.T
 
+# multiply by 1e-3 the second row
+data[1] = data[1] * 1e-3
+
 # save to the .txt file
 np.savetxt(save_path, data, fmt='%f', delimiter=' ')
