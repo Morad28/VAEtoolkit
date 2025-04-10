00662147faabcda8d60376f0f96e924324ae2dea
VictorDCh <vdubuschanson@outlook.fr>
2025-04-10 17:52:18 +0200

    Files to test on dopplight

--- utils/convert_dat_to_dopplight.py
+++ utils/convert_dat_to_dopplight.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-data_path = "testing_coils\std_dyn-shell_41_latent_5_kl_0.001_256_1D-COILS-GAIN_gw_0.1_phys_0.1_epochs_1000\laser_shot_g60_73.dat"
-save_path = "./pitch_profiles/pitch_g_60_73.txt"
+data_path = "testing_coils\std_dyn-shell_41_latent_5_kl_0.001_256_1D-COILS-GAIN_gw_0.1_phys_0.1_epochs_1000\laser_shot_g60_25.dat"
+save_path = "./pitch_profiles/pitch_g_60_25.txt"
 
 # load the data from the .dat file
 data = np.loadtxt(data_path, skiprows=1)
@@ -9,7 +9,7 @@ data = np.loadtxt(data_path, skiprows=1)
 # transpose the data
 data = data.T
 
-# multiply by 1e-3 the second row
+# multiply by 1e-3 the second row to get the pitch in meters
 data[1] = data[1] * 1e-3
 
 # save to the .txt file
