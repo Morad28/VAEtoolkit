84107836b2a6e56dfc8a55436704acae06ebf144
VictorDCh <vdubuschanson@outlook.fr>
2025-05-05 11:14:26 +0200

    profile to test

--- utils/convert_dat_to_dopplight.py
+++ utils/convert_dat_to_dopplight.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-data_path = "testing_coils_multi_6000/std_dyn-shell_42_latent_5_kl_1e-05_256_COILS-MULTI_gw_10_e99_e95_phys_0_epochs_2000/laser_shot_e37.dat"
-save_path = "pitch_profiles\DB2_profile\laser_shot_e37.txt"
+data_path = "testing_coils_multi_6000/std_dyn-shell_41_latent_10_kl_1e-05_256_COILS-MULTI-OUT-DUO_gw_10_gl_1_rl_1_e99_phys_0_epochs_2000/laser_shot_e39.dat"
+save_path = "pitch_profiles\DB2_profile\laser_shot_e39.txt"
 
 # load the data from the .dat file
 data = np.loadtxt(data_path, skiprows=1)
